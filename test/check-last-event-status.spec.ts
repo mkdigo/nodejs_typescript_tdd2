e895d626d6ac03eec8b4da27771740ad1d36f785
Rodrigo Mukudai <mkdigo@gmail.com>
2021-12-12 03:33:22 +0900

    rename usecase to SUT

--- test/check-last-event-status.spec.ts
+++ test/check-last-event-status.spec.ts
@@ -26,11 +26,10 @@ class LoadLastEventRepositoryMock implements LoadLastEventRepository {
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
     const loadLastEventRepository = new LoadLastEventRepositoryMock();
-    const checkLastEventStatus = new CheckLastEventStatus(
-      loadLastEventRepository
-    );
+    // System Under Test
+    const sut = new CheckLastEventStatus(loadLastEventRepository);
 
-    await checkLastEventStatus.perform('any_group_id');
+    await sut.perform('any_group_id');
 
     expect(loadLastEventRepository.groupId).toBe('any_group_id');
     expect(loadLastEventRepository.callsCount).toBe(1);
